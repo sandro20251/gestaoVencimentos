5b0edfebbf349e2010ea8a028f71c4aeed2f91bc
Sandro <sandrocoelho2023a@gmail.com>
2026-03-19 15:13:45 -0300

    adicionando lógica de cadastro de vencimento

--- rotas/vencimentos.js
+++ rotas/vencimentos.js
@@ -11,4 +11,25 @@ router.get('/cadastrar/:id', async (req, res) => {
     res.render('cadastrarVencimentos', { id });
 })
 
+// rota para cadstrar novo vencimetno
+
+router.post('/add/:id', async (req, res) => {
+    const id = req.params.id;
+    const ean = req.body.ean;
+    const produto = req.body.produto;
+    const fornecedor = req.body.fornecedor;
+    const quantidade = req.body.quantidade;
+    const data = req.body.date;
+
+    await Vencimentos.create({
+        ean: ean,
+        produto: produto,
+        fornecedor: fornecedor,
+        quantidade: quantidade,
+        data: data,
+        UsuarioId: id
+    })
+    res.redirect('/');
+})
+
 module.exports = router;
